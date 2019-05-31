ffcd74acf8e80c935a216b12845442ba181d09c8
jshang123 <1>
2019-05-31 10:33:23 +0800

    modify training procedure

--- code/run_alternative.sh
+++ code/run_alternative.sh
@@ -1,6 +1,11 @@
-python run_pretraining.py --model_name GBert-pretraining --num_train_epochs 10 --do_train --graph
-python run_gbert.py --model_name GBert-predict --use_pretrain --pretrain_dir ../saved/GBert-pretraining --num_train_epochs 10 --do_train --graph
-python run_pretraining.py --model_name GBert-pretraining --use_pretrain --pretrain_dir ../saved/GBert-predict --num_train_epochs 10 --do_train --graph
-python run_gbert.py --model_name GBert-predict --use_pretrain --pretrain_dir ../saved/GBert-pretraining --num_train_epochs 20 --do_train --graph
-python run_pretraining.py --model_name GBert-pretraining --use_pretrain --pretrain_dir ../saved/GBert-predict --num_train_epochs 10 --do_train --graph
-python run_gbert.py --model_name GBert-predict --use_pretrain --pretrain_dir ../saved/GBert-pretraining --num_train_epochs 30 --do_train --graph
+#! /bin/bash
+python run_pretraining.py --model_name GBert-pretraining-q --num_train_epochs 5 --do_train --graph
+python run_gbert.py --model_name GBert-predict-q --use_pretrain --pretrain_dir ../saved/GBert-pretraining-q --num_train_epochs 5 --do_train --graph
+
+for i in {1..15}
+do
+python run_pretraining.py --model_name GBert-pretraining-q --use_pretrain --pretrain_dir ../saved/GBert-predict-q --num_train_epochs 5 --do_train --graph
+python run_gbert.py --model_name GBert-predict-q --use_pretrain --pretrain_dir ../saved/GBert-pretraining-q --num_train_epochs 5 --do_train --graph
+done
+
+
